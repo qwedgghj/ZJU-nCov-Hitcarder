5b82f25ff8b038aed2bd8c2b770fbee57b994f17
qwedgghj <49071440+qwedgghj@users.noreply.github.com>
2022-05-04 16:26:02 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -187,8 +187,8 @@ def main(username, password):
 
     try:
         ret = hit_carder.check_form()
-#         if not ret:
-#             return 2, '打卡信息已改变，请手动打卡'
+        if not ret:
+            return 2, '打卡信息已改变，请手动打卡'
     except Exception as err:
         return 1, '获取信息失败，请手动打卡: ' + str(err)
 
